b3b2770bd21fb4e2e484c28d0b37501c7396903a
Kalin <kalin.trajkov@gmail.com>
2017-09-06 12:39:56 +0300

    Test

--- IoT.UnitTests/UnitTest1.cs
+++ IoT.UnitTests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace Iot.UnitTests
 {
@@ -8,6 +9,7 @@ namespace Iot.UnitTests
         [TestMethod]
         public void TestMethod1()
         {
+            throw new Exception();
         }
     }
 }
