0f51916a22c42f1120d95414d8bd75a42cdede5f
Kalin <kalin.trajkov@gmail.com>
2017-09-18 15:27:20 +0300

    Add ESP8266 and IoT.WebApi projects

--- IoT.Web/Startup.cs
+++ IoT.Web/Startup.cs
@@ -8,11 +8,25 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System.Globalization;
 using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace IoT.Web
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                             .SetBasePath(env.ContentRootPath)
+                             .AddJsonFile("appsettings.json")
+                             .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        } 
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -20,17 +34,17 @@ namespace IoT.Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }  
             app.Run(async (context) =>
-            {
-               
-              
-                await context.Response.WriteAsync("Hello");
+            {            
+                await context.Response.WriteAsync(Configuration["ResponseText"]);
             });
         }
     }
